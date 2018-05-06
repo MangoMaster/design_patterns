c7a5467551510c68f4ec5d32cebf543dc05d903b
mango <weijd17@mails.tsinghua.edu.cn>
2018-05-06 21:48:04 +0800

    decorator

---
+++ decorator/IO decorator/LowercaseInputStream.java
@@ -0,0 +1,27 @@
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LowercaseInputStream extends FilterInputStream {
+
+    public LowercaseInputStream(InputStream in) {
+        super(in);
+    }
+
+    public int read() throws IOException {
+        int c = super.read();
+        if (c == -1)
+            return c;
+        else
+            return Character.toLowerCase((char) c);
+    }
+
+    public int read(byte[] b, int offset, int len) throws IOException {
+        int result = super.read(b, offset, len);
+        for (int i = offset; i < offset + result; ++i) {
+            b[i] = (byte) Character.toLowerCase((char) b[i]);
+        }
+        return result;
+    }
+
+}

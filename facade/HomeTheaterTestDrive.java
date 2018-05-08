5acfb94f6c52e4e5355087bbca311e1006980dd9
mango <weijd17@mails.tsinghua.edu.cn>
2018-05-08 17:57:24 +0800

    adapter & facade

---
+++ facade/HomeTheaterTestDrive.java
@@ -0,0 +1,9 @@
+public class HomeTheaterTestDrive {
+
+    public static void main(String[] args) {
+        HomeTheaterFacade homeTheater = new HomeTheaterFacade(amp, tuner, dvd, cd, projector, screen, lights, popper);
+
+        homeTheater.watchMovie("Raiders of the Lost Ark");
+        homeTheater.endMovie();
+    }
+}
